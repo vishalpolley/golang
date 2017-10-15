450a7ce735b276265ebf4c9b0581cb813a9f7161
vishalpolley <vishalpolley290996@gmail.com>
2017-10-15 10:52:49 +0530

    Uploaded codes.

---
+++ variables.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+var pi float32 = 3.14
+func main() {
+	var (
+		a = 5
+		b = 10
+		c = 15
+	)
+	name := "Vishal Polley"
+	fmt.Printf("a is %d, b is %d and c is %d\n", a, b, c)
+	fmt.Println("The value of pie is", pi)
+	fmt.Println("My name is", name)
+}
